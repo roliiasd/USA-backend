4f8bc1a50c9670de9765cd034ba0c3acf13e31e0
Nimrod-v1 <meszaros.nimrod.383@dszcbaross.edu.hu>
2026-03-03 17:52:21 +0100

    switched Uid to user_id in db and made /animal/addanimal work

--- controllers/usedAnimalsContreoller.js
+++ controllers/usedAnimalsContreoller.js
@@ -1,14 +1,19 @@
+
 const {createanim} = require("../models/usedAnimalsModel")
 
 
 async function addanim(req,res) {
     try {
+        // console.log("req.user:", req.user)
+        // console.log("req.user.user_id:", req.user?.user_id)
         const {nev,kep,varos,megjegyzes} = req.body
-        const user_id = req.user.user_id
+        
+        
+        
         if (!nev || !kep || !varos || !megjegyzes) {
             return res.status(400).json({error:"nev, kep, varos, megjegyzes kell"})
         }
-        const {animalId} = await createanim(user_id,nev,kep,varos,megjegyzes)
+        const {animalId} = await createanim(req.user.user_id,nev,kep,varos,megjegyzes)
 
 
             return res.status(200).json({message:"Sikeres feltöltés",animalId})
