0930dfc516f6875f87d1193a35fdb570d56b27a2
Nimrod-v1 <meszaros.nimrod.383@dszcbaross.edu.hu>
2026-03-05 10:35:41 +0100

    Multer stuff || Upload middleware and file sorting in /uploads

--- routes/usedanimalsRoutes.js
+++ routes/usedanimalsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const {auth} = require('../middleware/userMiddleware.js')
+const {upload} = require('../middleware/uploadMiddleware.js')
 const {addanim,getanim} = require('../controllers/usedAnimalsContreoller.js')
 
 const router = express.Router()
 
-router.post('/addanimal',auth,addanim)
+router.post('/addanimal',auth,upload.single("kep"),addanim)
 router.get('/getanimals',getanim)
 
 
