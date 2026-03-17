8ab513a5a401ab7a98e08835f4901add9bfc650e
diak <diak@mesterlevel.lo>
2026-03-17 08:31:52 +0100

    new host

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const {config} = require('../config/dotenvConfig')
 
 
 const db = mysql.createPool({
-    host: config.HOST,
+    host: config.DB_HOST,
     user: config.DB_USER,
     password: config.DB_PASSWORD,
     database: config.DB_NAME,
